e141f22aceae1a86ff074ba4d542d6b03856351d
Mika <m.tores63@outlook.fr>
2026-03-06 17:04:50 +0100

    COMPLETED: form contact page just test but problem with turbo no redirection and no adding in bdd

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+
+        $contact = new Contact();
+        $formContact = $this->createForm(ContactType::class, $contact);
+        $formContact->handleRequest($request);
+
+        if ($formContact->isSubmitted() && $formContact->isValid()) {
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $formContact->createView(),
         ]);
     }
 }
